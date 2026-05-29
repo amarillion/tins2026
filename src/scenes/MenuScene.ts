65122824128ead06de913f1612b3dcac736b1c27
Martijn van Iersel <mvaniersel@gmail.com>
2026-05-29 17:10:14 +0200

    Working button in menu

--- src/scenes/MenuScene.ts
+++ src/scenes/MenuScene.ts
@@ -1,5 +1,18 @@
 import Phaser from 'phaser';
 
+class Button {
+	constructor(x: number, y: number, label: string, scene: Phaser.Scene, callback: () => void) {
+		const button = scene.add.text(x, y, label);
+		button
+			.setOrigin(0.5, 0.5)
+			.setPadding(10)
+			.setStyle({ backgroundColor: '#111' })
+			.setInteractive({ useHandCursor: true })
+			.on('pointerdown', () => callback())
+			.on('pointerover', () => button.setStyle({ fill: '#f39c12' }))
+			.on('pointerout', () => button.setStyle({ fill: '#fff' }));
+	}
+}
 export default class extends Phaser.Scene {
 
 	constructor() {
@@ -9,9 +22,7 @@ export default class extends Phaser.Scene {
 	create() {
 		this.cameras.main.setBackgroundColor('#ff0000');
 
-		const startButton = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, "Start Game");
-		// TODO: styling
-		startButton.addListener('click', () => {
+		new Button(this.cameras.main.centerX, this.cameras.main.centerY, "Start Game", this, () => {
 			console.log("Click!");
 			this.scene.start('Maze');
 		});
